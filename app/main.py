1edc00a58d5b7b33cc61791e68842c54411ddb0e
Mohammad Meraj <143806786+Meraj1312@users.noreply.github.com>
2026-03-27 11:33:51 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
-from app.api.ingestion import router
-from app.core.event_store import get_all_events
+from app.api.ingestion import router as ingestion_router
+from app.api.events import router as events_router
+from app.api.alerts import router as alerts_router
+from app.api.users import router as users_router
 
 app = FastAPI()
 
-app.include_router(router)
-
-
-@app.get("/events")
-def get_events():
-    events = get_all_events()
-    return {
-        "count": len(events),
-        "events": events
-    } 
+app.include_router(ingestion_router)
+app.include_router(events_router)
+app.include_router(alerts_router)
+app.include_router(users_router)
